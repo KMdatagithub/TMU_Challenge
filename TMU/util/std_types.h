65b85adb5127613cc2dd14beb55d62f5d46f512b
KMdatagithub <khaledelrawy9000@yahoo.com>
2020-02-25 21:06:25 +0200

    Update std_types.h

--- TMU/util/std_types.h
+++ TMU/util/std_types.h
@@ -13,20 +13,18 @@
 /*                   typedefs for standard types                        */
 /************************************************************************/
 
-#define NULL	((void *)0)
-
-typedef unsigned char uint8_t;
-typedef unsigned int uint16_t;
-typedef unsigned long int uint32_t;
-typedef unsigned long long uint64_t;
-typedef signed char sint8_t;
-typedef signed int sint16_t;
-typedef signed long int sint32_t;
-typedef signed long long sint64_t;
-typedef volatile uint8_t* const reg_type8_t;
-typedef volatile uint16_t* const reg_type16_t;
-
-typedef void (*FunPtr)(void);
+#define		NULL						((void *)0)
+typedef		unsigned char				uint8_t;
+typedef		unsigned int				uint16_t;
+typedef		unsigned long int			uint32_t;
+typedef		unsigned long long			uint64_t;
+typedef		signed char					sint8_t;
+typedef		signed int					sint16_t;
+typedef		signed long int				sint32_t;
+typedef		signed long long			sint64_t;
+typedef		volatile uint8_t* const		reg_type8_t;
+typedef		volatile uint16_t* const	reg_type16_t;
+typedef		void (*FunPtr)(void);
 
 
 /************************************************************************/
