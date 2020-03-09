ea4e59bf213aa82b3541fe4f54ea6dc0af375236
KMdatagithub <khaledelrawy9000@yahoo.com>
2020-03-09 18:20:01 +0200

    CPU Load Calculated

--- TMU/Debug/main.d
+++ TMU/Debug/main.d
@@ -2,9 +2,22 @@ main.d main.o: .././main.c .././SL/SOS.h .././SL/../MCAL/Timer.h \
  .././SL/../MCAL/../util/std_types.h .././SL/../MCAL/../util/registers.h \
  .././SL/../MCAL/../util/std_types.h .././SL/../MCAL/../util/interrupt.h \
  .././SL/../MCAL/../util/registers.h .././SL/../util/std_types.h \
- .././SL/../util/interrupt.h .././SL/../util/error_codes.h \
+ .././SL/../util/interrupt.h .././SL/../util/error_codes.h .././SL/BCM.h \
  .././MCAL/DIO.h .././MCAL/../util/std_types.h \
- .././MCAL/../util/registers.h
+ .././MCAL/../util/registers.h .././MCAL/UART.h \
+ .././MCAL/../util/interrupt.h .././MCAL/UART_cfg.h .././MCAL/UART.h \
+ e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sleep.h \
+ e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
+ e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ E:\Prog_Files\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iom32.h \
+ e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
 
 .././SL/SOS.h:
 
@@ -26,8 +39,42 @@ main.d main.o: .././main.c .././SL/SOS.h .././SL/../MCAL/Timer.h \
 
 .././SL/../util/error_codes.h:
 
+.././SL/BCM.h:
+
 .././MCAL/DIO.h:
 
 .././MCAL/../util/std_types.h:
 
 .././MCAL/../util/registers.h:
+
+.././MCAL/UART.h:
+
+.././MCAL/../util/interrupt.h:
+
+.././MCAL/UART_cfg.h:
+
+.././MCAL/UART.h:
+
+e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sleep.h:
+
+e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
+
+e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+E:\Prog_Files\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iom32.h:
+
+e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+e:\prog_files\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
--- TMU/main.c
+++ TMU/main.c
@@ -6,13 +6,56 @@
  */ 
 
 #include "SL/SOS.h"
+#include "SL/BCM.h"
 #include "MCAL/DIO.h"
+#include "MCAL/UART.h"
+#include <avr/sleep.h>
 
+/**=========================================================================*
+ *								Defines & Globals							*
+ *==========================================================================*/
+
+#define TRANSMIT_MAX 500
+#define ENTER_KEY	 0x0D
+
+BCM_cfg_s BCM1;
+UART_cfg UART1;
 DIO_Cfg_s g_LED1;
 DIO_Cfg_s g_LED2;
 DIO_Cfg_s g_LED3;
+DIO_Cfg_s Test_Pin;
 TMU_cfg_s TMU1;
 
+uint8_t txBuffer[TRANSMIT_MAX];
+uint8_t msg_len = ZERO;
+
+volatile uint8_t a_index        = ZERO;
+volatile uint8_t g_TxBuffer_Len = ZERO;
+volatile uint8_t g_UART_TXindex = ZERO;
+volatile uint8_t BCM_sending    = FALSE;
+
+/**=========================================================================*
+ *							BCM Notification Functions						*
+ *==========================================================================*/
+
+/* TX Completion Notification Routine */
+void txnotify(enum_BcmStatus st)
+{
+	BCM_sending = FALSE ;
+	g_UART_TXindex = ZERO;
+	BCM_DeInit(&BCM1);
+}
+
+/*==========================================================================*/
+
+void CPU_Sleep(void)
+{
+	sleep_enable();
+	sei();
+	sleep_cpu();
+}
+
+/*==========================================================================*/
 
 void ToggleLED1()
 {
@@ -26,13 +69,7 @@ void ToggleLED3()
 {
 	DIO_Toggle(g_LED3.GPIO, g_LED3.pins);
 }
-
-void StopTasks()
-{
-	TMU_Stop(100);
-	//TMU_Stop(110);
-}
-
+;
 void Init_LEDs(void)
 {
 	g_LED1.GPIO = GPIOD;
@@ -48,28 +85,45 @@ void Init_LEDs(void)
 	g_LED3.pins = BIT6;
 	DIO_init(&g_LED3);
 	DIO_Write(g_LED3.GPIO, g_LED3.pins, HIGH);
+	Test_Pin.dir = OUTPUT;
+	Test_Pin.GPIO = GPIOD;
+	Test_Pin.pins = BIT0;
+	DIO_init(&Test_Pin);
 }
 
+/*==========================================================================*/
 
 int main(void)
 {
+	/*-------------[ LEDs Initialization ]-------------*/
+	Init_LEDs();
+		
 	/*-------------[ TMU Initialization ]-------------*/
 	TMU1.Tick_Time = 1;
 	TMU1.Timer_ID = TIMER_CH0;
 	TMU_Init(&TMU1);
-	
-	/*-------------[ LEDs Initialization ]-------------*/
-	Init_LEDs();
 	
 	/*----------[ Start 3 Different Consumers ]---------*/
 	/*[[ TMU_Srart(Consumer_FunPtr, ConsumerID, Periodicity, Time_IN_ms); ]]*/
 	TMU_Start(ToggleLED1, 100, PERIODIC, 100);
 	TMU_Start(ToggleLED2, 110, PERIODIC, 300);
 	TMU_Start(ToggleLED3, 120, ONESHOT, 3000);
-	TMU_Start(StopTasks, 130, ONESHOT, 5000);
 	
+	/*-------------[ BCM Initialization ]-------------*/
+	BCM1.BCM_CH_ID = 1;
+	BCM1.Mode = BCM_Tx_Mode;
+	BCM1.Protocol = SPI_Protocol;
+	BCM_Init(&BCM1);
+	BCM_Send(txBuffer, TRANSMIT_MAX, &BCM1, txnotify);
+	
+	/*-------------[ SUPER LOOP ]-------------*/
 	while (1)
 	{
+		DIO_Write(Test_Pin.GPIO, Test_Pin.pins, HIGH);
+		BCM_Tx_Dispatcher();
 		TMU_Dispatcher();
+		DIO_Write(Test_Pin.GPIO, Test_Pin.pins, LOW);
+		CPU_Sleep();
+		// CPU Load Is Found To Be 3% At The First Few Seconds And It Drops Down To 2% Thereafter
 	}
 }
